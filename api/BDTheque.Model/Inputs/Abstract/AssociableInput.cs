afcb11f479d8e0d32152a8a715d69330c00a274a
Tetram <tetram76@gmail.com>
2024-02-23 17:04:39 +0100

    fix unit tests

--- api/BDTheque.Model/Inputs/Abstract/AssociableInput.cs
+++ api/BDTheque.Model/Inputs/Abstract/AssociableInput.cs
@@ -2,5 +2,5 @@ namespace BDTheque.Model.Inputs;
 
 public abstract class AssociableInput : UniqueIdInput
 {
-    public Optional<IList<string>?> Associations { get; set; }
+    public Optional<IList<string>> Associations { get; set; }
 }
