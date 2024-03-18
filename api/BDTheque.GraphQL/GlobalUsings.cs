b651fe84d80980814d230a2caf66f2e77a0b7ad1
Tetram <tetram76@gmail.com>
2024-03-18 21:28:23 +0100

    clean deps

--- api/BDTheque.GraphQL/GlobalUsings.cs
+++ api/BDTheque.GraphQL/GlobalUsings.cs
@@ -1,5 +1,7 @@
 global using System.Diagnostics.CodeAnalysis;
-global using HotChocolate;
+
 global using BDTheque.Model.Entities;
 global using HotChocolate.Types;
+global using HotChocolate.Types.Relay;
+global using HotChocolate;
 global using Microsoft.EntityFrameworkCore;
